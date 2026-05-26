9083d7403b79213fc6be3239dfa7531298fc4b49
Balakrishna Marripudi <marripudi.balakrishna@gmail.com>
2026-05-26 18:20:37 +0530

    globalerrorhandlermiddlewarecreating

---
+++ Employeecontroller/Middleware/AppException.cs
@@ -0,0 +1,15 @@
+﻿namespace Employeecontroller.Middleware
+{
+    public class AppException: Exception
+    {
+        public AppException():base()
+        {
+        }
+        public AppException(string message):base(message)
+        {
+        }
+         public AppException(string message, Exception innerException):base(message,innerException)
+        {
+        }
+    }
+}
---
+++ Employeecontroller/Middleware/GlobalErrorHandlerMiddleware.cs
@@ -0,0 +1,27 @@
+﻿namespace Employeecontroller.Middleware
+{
+    public class GlobalErrorHandlerMiddleware
+    {
+
+        public readonly RequestDelegate _next;
+        public GlobalErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                //context.Response.StatusCode = 500;
+                //context.Response.ContentType = "application/json";
+                //var errorResponse = new { message = "An unexpected error occurred.", details = ex.Message };
+                //await context.Response.WriteAsJsonAsync(errorResponse);
+            }
+        }
+    }
+}
